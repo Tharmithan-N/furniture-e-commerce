d9e62bb222bcaa557d245b87e0437f14cf6f5f97
Tharmithan <tharmithan@orelit.com>
2024-12-05 10:58:00 +0530

    navbar wrapper created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import TopNavBar from "./layouts/topNavBar";
+import HeroBanner from "./pages/Home/HeroBanner";
 
 export default function App() {
   return (
     <div>
-      <TopNavBar />
+      <TopNavBar>
+        <HeroBanner />
+      </TopNavBar>
     </div>
   )
 }
