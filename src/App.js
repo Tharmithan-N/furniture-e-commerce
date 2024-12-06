543e07922ce0acd8eea3426db62bfe823202e0d1
Tharmithan <tharmithan@orelit.com>
2024-12-06 16:50:00 +0530

    Home page done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import BtmFooter from "./layouts/btmFooter";
 import TopNavBar from "./layouts/topNavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
 
 export default function App() {
   return (
-    <div>
-      <BtmFooter>
-        <TopNavBar>
-          <Home />
-        </TopNavBar>
-      </BtmFooter>
-    </div>
+    <Router>
+      <div>
+        <BtmFooter>
+          <TopNavBar>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/about" element={<Shop />} />
+              <Route path="/contact" element={<Shop />} />
+            </Routes>
+          </TopNavBar>
+        </BtmFooter>
+      </div>
+    </Router>
   );
 }
--- src/layouts/topNavBar.jsx
+++ src/layouts/topNavBar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import furniroLogo from "../assets/layouts/furniroLogo.png";
 import { CiUser, CiSearch, CiHeart, CiShoppingCart } from "react-icons/ci";
+import { useNavigate } from "react-router-dom";
 
 const TopNavBar = ({ children }) => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col min-h-screen">
       {/* Top Navigation Bar */}
       <div className="flex justify-between items-center my-[8px]">
-        <div className="flex gap-[5px] ml-[54px] items-center cursor-pointer">
+        <div className="flex gap-[5px] ml-[54px] items-center cursor-pointer" onClick={()=> navigate("/")}>
           <div>
             <img src={furniroLogo} alt="furniroLogo" />
           </div>
@@ -18,6 +20,7 @@ const TopNavBar = ({ children }) => {
             <div
               key={item}
               className="font-medium text-[16px] cursor-pointer relative group"
+              onClick={() => navigate(item === "Home" ? "/" : `/${item.toLowerCase()}`)}
             >
               {item}
               <span className="absolute left-0 bottom-[-2px] w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
